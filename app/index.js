2a92c125b1aceee249771902693b84ce8365bc41
Dimas <dimsrad307@gmail.com>
2016-11-26 23:08:34 +0700

    Added avatar component by using multiple components

--- app/index.js
+++ app/index.js
@@ -1,12 +1,47 @@
+var USER_DATA = {
+    fullName: 'Dimas Radityo',
+    userName: 'dimasradityo',
+    imageUrl: 'http://placehold.it/350x350'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfileName = React.createClass({
+    render: function () {
+        return <div> {this.props.name} </div>
+        
+    }
+});
+
+var ProfileUser = React.createClass({
     render: function(){
-        return(
-            <div> Hello </div>
+        return (
+            <div>
+                <a href={'https://github.com/' + this.props.username}>
+                    {this.props.username}
+                </a>
+            </div>
         )
     }
 });
 
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+var ProfileImage = React.createClass({
+    render: function (){
+        return <img src={this.props.image} style={{height: 100, width: 100}}/>        
+    }
+});
+
+var Avatar = React.createClass({
+    render: function(){
+        return (
+        <div>
+            <ProfileImage image = {this.props.user.imageUrl}/>
+            <ProfileName name = {this.props.user.fullName}/>
+            <ProfileUser username = {this.props.user.userName}/>            
+        </div>
+        )
+    }
+});
+
+ReactDOM.render(<Avatar user={USER_DATA}/>, document.getElementById('app'));
